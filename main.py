4593dc5ec09a6987e904f68f5986c0ba27b516ac
rlemos-dev <rlemos.dev01@gmail.com>
2026-04-27 22:49:06 -0300

    add nova verificaçao 2fa

--- main.py
+++ main.py
@@ -35,9 +35,33 @@ botao.click()
 
 time.sleep(5)
 
-acesso_url(driver, username)
+verif= input('Sua conta tem autenticação de dois fatores'
+'(Responda com S/N)' \
+':')
 
 time.sleep(5)
+
+if verif in ('S', 's'):
+
+    fa= driver.find_element(By. XPATH,'//input[contains(@class, "x1i10hfl xggy1nq xtpw4lu x1tutvks x1s3xk63 x1s07b3s x1a2a7pz xjbqb8w x1v8p93f ' \
+    'x1o3jo1z x16stqrj xv5lvn5 x1ejq31n x18oe1m7 x1sy0etr xstzfhl x972fbf x10w94by x1qhh985 x14e42zd x9f619 xzsf02u x1lliihq x15h3p50 x10emqs4 ' \
+    'x1vr9vpq x1iyjqo2 x10d0gm4 x1fhayk4 x16wdlz0 x3cjxhe xe9ewy2 x11lt19s xeuugli xlyipyv x1hcrkkg xfvqz1d x12vv892 x1hu168l xttzon8 x1sfh74k ' \
+    'x3fqe8q x185fvkj x1p97g3g xmtqnhx x11ig0mb xgmu6d7 x1quw8ve xx0ingd xp5op4 x1y44fgy xdzva22 xs8nzd4 x1fzehxr xha3pab" )]')
+
+    time.sleep(5)
+
+    fa.send_keys(input('Digite o Código de 2 Fatores da sua conta:'))
+
+    time.sleep(5)
+
+botao2 = driver.find_element(By.XPATH, '//span[text()="Continuar"]/ancestor::*[@role="button"]')
+botao2.click()
+
+time.sleep(5)
+
+acesso_url(driver, username)
+
+time.sleep(10)
 
 lista_seguidores= []
 seguidores= driver.find_element(By.XPATH, '//a[contains(@href, "/followers/")]')
